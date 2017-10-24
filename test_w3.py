bf514c4969fdb568a6eb094035b653b7a8128a9e
Alexey Sharonov <sharonov-azlk@mail.ru>
2017-10-24 05:38:01 +0000

    com

--- test_w3.py
+++ test_w3.py
@@ -73,34 +73,15 @@ myAddress = pywaves.Address(seed=seed_str)
 
 #print(myAddress.balance())
 #myAddress.sendWaves(recipient = pywaves.Address('3NBVqYXrapgJP9atQccdBPAgJPwHDKkh6A8'), amount = 300000000)
-
-#list of components in bytes to be assembled in transaction or block etc.
-comp = {}
-
+time = int(time.time() * 1000)
 #constructing Tx
-comp = {    't_type' : b'\x04',
-            'k_pub' : b58decode('EENPV1mRhUD9gSKbcWt84cqnfSGQP5LkCu5gMBfAanYH'),
-            'amount_flag' : bytes([1]), # 0 - waves
-            'as_ID' : b58decode('BG39cCNUFWPQYeyLnu7tjKHaiUGRxYwJjvntt9gdDPxG'), # Amount's asset ID (if used)
-            'fee_flag' : bytes([1]), # Fee's asset flag (0-Waves, 1-Asset)
-            'fee_ID' : b58decode('BG39cCNUFWPQYeyLnu7tjKHaiUGRxYwJjvntt9gdDPxG'), # Fee's asset ID (if used)
-            'timestamp' : (1479287120875).to_bytes(8, byteorder='big'), #timestamp = int(time.time() * 1000)
-            'amount' : (1).to_bytes(8, byteorder='big'), # 1 stands for amount
-            'fee' : (1).to_bytes(8, byteorder='big'), # 1 stands for fee
-            'recip_address' : b58decode('3NBVqYXrapgJP9atQccdBPAgJPwHDKkh6A8'),
-            'att' : (4).to_bytes(2, byteorder='big'),
-            'att_bytes' : b58decode('2VfUX')
-        }
-
-comp = {    't_type' : b'\x04',
+comp = {    't_type' : b'\4',
             'k_pub' : k_pub,
-            'amount_flag' : bytes([0]), # 0 - waves
-            #'as_ID' : b58decode('BG39cCNUFWPQYeyLnu7tjKHaiUGRxYwJjvntt9gdDPxG'), # Amount's asset ID (if used)
-            'fee_flag' : bytes([0]), # Fee's asset flag (0-Waves, 1-Asset)
-            #'fee_ID' : b58decode('BG39cCNUFWPQYeyLnu7tjKHaiUGRxYwJjvntt9gdDPxG'), # Fee's asset ID (if used)
-            'timestamp' : int(time.time() * 1000).to_bytes(8, byteorder='big'), #'timestamp' : (1479287120875).to_bytes(8, byteorder='big')
-            'amount' : (1).to_bytes(8, byteorder='big'), # amount = 1
-            'fee' : (1).to_bytes(8, byteorder='big'), # fee = 1
+            'amount_flag' : b'\0',
+            'fee_flag' : b'\0',
+            'timestamp' : time.to_bytes(8, byteorder='big'), #'timestamp' : (1479287120875).to_bytes(8, byteorder='big')
+            'amount' : (100000000).to_bytes(8, byteorder='big'),
+            'fee' : (100000).to_bytes(8, byteorder='big'),
             'recip_address' : b58decode('3NBVqYXrapgJP9atQccdBPAgJPwHDKkh6A8'),
             'att' : (4).to_bytes(2, byteorder='big'),
             'att_bytes' : b58decode('2VfUX')
@@ -111,27 +92,24 @@ for key, value in comp.items():
     #print("{0}: {1}".format(key, b58encode(value)))
     tx += value
 
+#print(b58encode(tx))
 randm64 = os.urandom(64)
 sign = curve.calculateSignature(randm64, k_pr, tx)
 #print(b58encode(sign))
-#sign = crypto.sign(b58encode(k_pr), tx)
-#print(sign)
-
-#print('result')
 #print(b58encode(tx + sign))
 
 data = json.dumps({
                 "senderPublicKey": b58encode(k_pub),
                 "recipient": '3NBVqYXrapgJP9atQccdBPAgJPwHDKkh6A8',
-                "amount": 300000000,
+                "amount": 100000000,
                 "fee": 100000,
-                "timestamp": int(time.time() * 1000),
+                "timestamp": time,
                 "attachment": '2VfUX',
                 "signature": b58encode(sign)
             })
-#print(data)
-#pywaves.wrapper('/assets/broadcast/transfer', postData=data, host='https://testnode4.wavesnodes.com')
-#myAddress.sendWaves(recipient = pywaves.Address('3NBVqYXrapgJP9atQccdBPAgJPwHDKkh6A8'), amount = 500000000)
 
 print(pywaves.wrapper('/addresses/balance/%s%s' % (b58encode(address), ''))['balance'])
-print(pywaves.wrapper('/assets/broadcast/transfer', postData=data, host='https://testnode4.wavesnodes.com'))
+#r = pywaves.wrapper('/assets/broadcast/transfer', postData=data, host='https://testnode4.wavesnodes.com')
+
+#req = requests.post('https://testnode1.wavesnodes.com/assets/broadcast/transfer', data=data, headers={'content-type': 'application/json'}).json()
+#print(req)#.status_code)
